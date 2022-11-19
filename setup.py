9a42e470da91fd6e602ac981c22dd01366b6422f
karishmas6 <carishmashukla28@gmail.com>
2022-11-20 04:18:13 +0530

    chore: rename to pypidisk

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def readme():
         README = f.read()
     return README
 
-VERSION = '1.0.4'
+VERSION = '1.0.0'
 DESCRIPTION = 'CLI tool to know the amount of disk space PyPi packages are occupying.'
 
 setup(
-    name="pydisk",
+    name="pypidisk",
     version=VERSION,
     author="Karishma Shukla",
     author_email="karishmashuklaa@gmail.com",
-    url="https://github.com/karishmashuklaa/pydisk",
+    url="https://github.com/karishmashuklaa/pypidisk",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description= readme(),
@@ -21,7 +21,7 @@ setup(
     keywords=['diskspace', 'dependencies', 'pypi', 'cli', 'pydisk', 'memory', 'modules', 'packages'],
     entry_points={
         'console_scripts': [
-            'pydisk=pydisk.__init__.py:get_package_size',
+            'pypidisk=pypidisk.__init__.py:get_package_size',
         ]
     },
     license="MIT",
